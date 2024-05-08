9b17e43cd691492071f180fce8b2fd7fd4063cab
shajwan2023 <133630848+shajwan2023@users.noreply.github.com>
2024-05-08 23:08:39 +0300

    Create DateInput.svelte

---
+++ src/lib/components/DateInput.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Label, Input } from "flowbite-svelte";
+
+  export let id;
+  export let label;
+  export let bind_value:any;
+</script>
+
+<div>
+  <Label for={id}>{label}</Label>
+  <Input type="date" id={id} bind:value={bind_value} />
+</div>
