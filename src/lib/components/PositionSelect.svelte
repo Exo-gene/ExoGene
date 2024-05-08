b4b94a077cde40f0b26593cd6c6a267d28a313a3
shajwan2023 <133630848+shajwan2023@users.noreply.github.com>
2024-05-08 23:09:07 +0300

    Create PositionSelect.svelte

---
+++ src/lib/components/PositionSelect.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Label, Select } from "flowbite-svelte";
+
+  export let positions:any = [];
+  export let selectPosition;
+  export let selectedPosition;
+</script>
+
+<div>
+  <Label for="position-select">Select Position</Label>
+  <Select id="position-select" on:change={selectPosition}>
+    {#each positions as position}
+      <option value={position} selected={position === selectedPosition}>
+        {position}
+      </option>
+    {/each}
+  </Select>
+</div>
