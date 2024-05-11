651e27e0cd73576515cf206d2cb102ad1cd6679f
shajwan2023 <133630848+shajwan2023@users.noreply.github.com>
2024-05-11 22:44:36 +0300

    Update LanguageTabs.svelte

--- src/lib/components/LanguageTabs.svelte
+++ src/lib/components/LanguageTabs.svelte
@@ -7,13 +7,13 @@
   export let handleFileChange;
 </script>
 
-<Tabs tabStyle="underline" defaultClass="bg-[#D0D0D0] flex">
+<Tabs tabStyle="underline" contentClass="tabs-background">
   {#each languages as language}
     <TabItem title={language} open={language === LanguageEnum.EN}>
-      <p class="text-red-600 mb-2">
+      <p class="text-red-600 mb-2 p-2">
         Note: Only one file (image or video) can be entered for each language.
       </p>
-      <div class="text-sm text-gray-500 dark:text-gray-400">
+      <div class="text-sm text-gray-500 dark:text-gray-400 p-2">
         <div class="mb-6 flex justify-between items-start">
           <div class="mb-6">
             <div>
@@ -67,3 +67,11 @@
     </TabItem>
   {/each}
 </Tabs>
+
+
+<style>
+  .tabs-background {
+    background-color: var(--background-color);
+  }
+</style>
+ 
