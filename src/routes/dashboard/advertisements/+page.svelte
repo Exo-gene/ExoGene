8b64f9c5c170b4d363ae2ab4d1a79b78c4db0143
shajwan2023 <133630848+shajwan2023@users.noreply.github.com>
2024-05-13 23:28:56 +0300

    Update +page.svelte

--- src/routes/dashboard/advertisements/+page.svelte
+++ src/routes/dashboard/advertisements/+page.svelte
@@ -1,25 +1,12 @@
 <script lang="ts">
   import InsertButton from "../../../lib/components/InsertButton.svelte";
-  import { goto } from "$app/navigation";
-  import {
-    Table,
-    TableBody,
-    TableBodyCell,
-    TableBodyRow,
-    TableHead,
-    TableHeadCell,
-  } from "flowbite-svelte";
+  import { goto } from "$app/navigation"; 
   import { onMount } from "svelte";
   import { supabase } from "$lib/supabaseClient";
-  import { advertisementStore } from "../../../stores/advertisementStore";
-  import { LanguageEnum } from "../../../models/languageEnum";
-  import { formatDateTime } from "$lib/utils/formatDateTime";
-  import ConfirmDeleteModal from "$lib/components/ConfirmDeleteModal.svelte";
-  import PaginationControls from "$lib/components/PaginationControls.svelte";
-  import IconTrash from "@tabler/icons-svelte/IconTrash.svelte";
-  import IconEdit from "@tabler/icons-svelte/IconEdit.svelte";
-  import LoadingIndicator from "$lib/components/LoadingIndicator.svelte";
-  import { toLocaleDate } from "$lib/utils/dateTimeFormat.js";
+  import { advertisementStore } from "../../../stores/advertisementStore"; 
+  import ConfirmDeleteModal from "$lib/components/ConfirmDeleteModal.svelte"; 
+  import LoadingIndicator from "$lib/components/LoadingIndicator.svelte"; 
+  import CustomTable from "$lib/components/CustomTable.svelte";
 
   let openModal = false;
   let itemIdToDelete: number | null = null;
@@ -91,56 +78,13 @@
   $: advertisements = $advertisementStore[0]?.items || [];
 </script>
 
+ 
 {#if isLoading}
   <LoadingIndicator />
-{:else if $advertisementStore[0]?.items.length > 0}
+{:else}
   <div class="mx-2">
-    <InsertButton insertData={createAdvertisement} />
-    <Table>
-      <TableHead
-        style="background-color: var(--tableHeaderBackgroundColor); color:var(--tableHeaderColor)"
-      >
-        <TableHeadCell class="!p-4"></TableHeadCell>
-        {#each tableHeaders as header}
-          <TableHeadCell>{header}</TableHeadCell>
-        {/each}
-      </TableHead>
-      <TableBody tableBodyClass="divide-y">
-        {#each advertisements as advertisementItem}
-          <TableBodyRow
-            style="background-color: var(--tableBackgroundColor); color:var(--tableColor)"
-          >
-            <TableBodyCell class="!p-4"></TableBodyCell>
-            <TableBodyCell>{advertisementItem.id}</TableBodyCell>
-            <TableBodyCell class="font-semibold text-gray-700">
-            {formatDateTime(advertisementItem.created_at.toString())}
-            </TableBodyCell>
-            {#each advertisementItem.advertisementtranslation as translation}
-              {#if translation.language === LanguageEnum.EN}
-                <TableBodyCell>
-                  <span>{translation.language}</span>
-                </TableBodyCell>
-              {/if}
-            {/each}
-            <TableBodyCell class="flex space-x-3">
-              <button
-                class="font-medium text-green-600 hover:underline dark:text-green-600"
-                on:click={() => editAdvertisement(advertisementItem.id)}
-              >
-                <IconEdit stroke={2} class="text-green-700" />
-              </button>
-              <button on:click={() => handleDelete(advertisementItem.id)}
-                ><IconTrash stroke={2} class="text-red-700" /></button
-              >
-            </TableBodyCell>
-          </TableBodyRow>
-        {/each}
-      </TableBody>
-    </Table>
-    <PaginationControls {currentPage} {totalPages} {previousPage} {nextPage} />
-    <ConfirmDeleteModal
-      bind:open={openModal}
-      on:confirm={deleteAdvertisement}
-    />
+   <InsertButton insertData={createAdvertisement} />
+   <CustomTable items={advertisements} editData={editAdvertisement} {handleDelete} {tableHeaders} {currentPage} {totalPages} {previousPage} {nextPage}/>
+   <ConfirmDeleteModal bind:open={openModal} on:confirm={deleteAdvertisement} />
   </div>
 {/if}
