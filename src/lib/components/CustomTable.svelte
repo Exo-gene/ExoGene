9d9164ee2b5345c0044f148f50d06fe4d9e5c305
shajwan2023 <133630848+shajwan2023@users.noreply.github.com>
2024-05-13 13:52:01 +0300

    Update CustomTable.svelte

--- src/lib/components/CustomTable.svelte
+++ src/lib/components/CustomTable.svelte
@@ -1,4 +1,6 @@
-<script lang="ts">
+
+
+  <script lang="ts">
   import {
     Table,
     TableBody,
@@ -7,55 +9,61 @@
     TableHead,
     TableHeadCell,
   } from "flowbite-svelte";
-  // import EditButton from "./EditButton.svelte";
+  import { LanguageEnum } from "../../models/languageEnum";
+  import { formatDateTime } from "$lib/utils/formatDateTime";
+  import IconTrash from "@tabler/icons-svelte/IconTrash.svelte";
+  import IconEdit from "@tabler/icons-svelte/IconEdit.svelte";
   import PaginationControls from "./PaginationControls.svelte";
 
-  export let tableHeaders: string[] = [];
-  export let items: any[] = [];
+  export let items :any= [];
+  export let editData: (itemId: number) => void;
+  export let handleDelete: (itemId: number) => void;
+  export let tableHeaders;
   export let currentPage: number;
   export let totalPages: number;
   export let previousPage: () => void;
-  export let nextPage: () => void;
-  export let onDelete: (itemId: number) => void;
+  export let nextPage: () => void; 
 
-  function handleDelete(itemId: number) {
-    onDelete(itemId);
-  }
- 
 </script>
 
-<div class="mx-2">
-  <div>
-    <Table>
-      <TableHead
-        style="background-color: var(--tableHeaderBackgroundColor); color:var(--tableHeaderColor)"
-      >
-        <TableHeadCell class="!p-4"></TableHeadCell>
-        {#each tableHeaders as header}
-          <TableHeadCell>{header}</TableHeadCell>
-        {/each}
-      </TableHead>
-      <TableBody>
-        {#each items as item}
-          <TableBodyRow
-            style="background-color: var(--tableBackgroundColor); color:var(--tableColor)"
-          >
-            <TableBodyCell class="!p-4"></TableBodyCell>
-            {#each Object.values(item) as value}
-              <TableBodyCell>{value}</TableBodyCell>
-            {/each}
-            <TableBodyCell class="flex space-x-3">
-              <!-- action buttons -->
-              <!-- <EditButton categoryId={item.id} pageLink="categories" /> -->
-              <button on:click={() => handleDelete(item.id)}>Delete</button>
+<Table>
+  <TableHead style="background-color: var(--tableHeaderBackgroundColor); color:var(--tableHeaderColor)">
+    <TableHeadCell class="!p-4"></TableHeadCell>
+    {#each tableHeaders as header}
+      <TableHeadCell>{header}</TableHeadCell>
+    {/each}
+  </TableHead>
+  <TableBody tableBodyClass="divide-y">
+    {#each items as item}
+      <TableBodyRow style="background-color: var(--tableBackgroundColor); color:var(--tableColor)">
+        <TableBodyCell class="!p-4"></TableBodyCell>
+        <TableBodyCell>{item.id}</TableBodyCell>
+        <TableBodyCell class="font-semibold text-gray-700">
+          {formatDateTime(item.created_at.toString())}
+        </TableBodyCell>
+        {#each item.translation as translation}
+          {#if translation.language === LanguageEnum.EN}
+            <TableBodyCell>
+              <span>{translation.title.slice(0, 40)}{translation.title.length > 40 ? '...' : ''}</span>
             </TableBodyCell>
-          </TableBodyRow>
+            <TableBodyCell>
+              <span>{translation.language}</span>
+            </TableBodyCell>
+          {/if}
         {/each}
-      </TableBody>
-    </Table>
-  </div>
-
-  <div>
-    <PaginationControls {currentPage} {totalPages} {previousPage} {nextPage} />
-  </div>
-</div>
+        <TableBodyCell class="flex space-x-3">
+          <button
+            class="font-medium text-green-600 hover:underline dark:text-green-600"
+            on:click={() => editData(item.id)}
+          >
+            <IconEdit stroke={2} class="text-green-700" />
+          </button>
+          <button on:click={() => handleDelete(item.id)}
+          ><IconTrash stroke={2} class="text-red-700" /></button
+          >
+        </TableBodyCell>
+      </TableBodyRow>
+    {/each}
+  </TableBody>
+</Table>
+ <PaginationControls {currentPage} {totalPages} {previousPage} {nextPage} />
