8a6826a90dbbf266edcd5e3b404763298e7cecdc
tiwarNews <168510639+tiwarNews@users.noreply.github.com>
2024-06-08 18:21:31 +0300

    Update +page.svelte

--- src/routes/dashboard/employees/add/+page.svelte
+++ src/routes/dashboard/employees/add/+page.svelte
@@ -49,9 +49,8 @@
  
 
 <div
-  class="border border-gray-800 p-10 rounded container mx-auto px-12 mt-12 w-3/5 float-right lg:float-none flex flex-col gap-4"
+  class="border border-gray-800 p-10 rounded max-w-xl mx-auto"
 >
-
 <div class="w-full h-auto flex justify-center items-center ">
   <p class="text-xl font-bold p-2">{"Add New Employee"}</p>
 </div>
@@ -59,7 +58,7 @@
 
   <p class="text-center">{"Add Your Image"}</p>
   <UploadImage bind:image={userOptions.image} />
-  <div class="w-full flex flex-col h-auto gap-2">
+  <div class="w-full flex flex-col h-auto gap-2 py-2">
     <p class=" w-full h-4 rounded-lg">{"Name"}</p>
     <input
       type="text"
@@ -68,7 +67,7 @@
       required
     />
   </div>
-  <div class="w-full flex flex-col h-auto gap-2">
+  <div class="w-full flex flex-col h-auto gap-2 py-2">
     <p class=" w-full h-4 rounded-lg">{"Email"}</p>
     <input
       type="text"
@@ -77,7 +76,7 @@
       required
     />
   </div>
-  <div class="w-full flex flex-col h-auto gap-2">
+  <div class="w-full flex flex-col h-auto gap-2 py-2">
     <p class=" w-full h-4 rounded-lg">{"Password"}</p>
     <input
       type="password"
@@ -86,7 +85,7 @@
       required
     />
   </div>
-  <div class="w-full flex flex-col h-auto gap-2">
+  <div class="w-full flex flex-col h-auto gap-2 py-2">
     <p class=" w-full h-4 rounded-lg ">{"Roles"}</p>
  
     <MultiSelect
@@ -111,7 +110,7 @@
     </button>
   {:else}
     <Button
-      class="w-full h-12  duration-300 ease-in-out rounded-lg text-white"
+      class="mt-5 w-full h-12  duration-300 ease-in-out rounded-lg text-white"
       on:click={() => {
         create(userOptions, userRoleOptions, password);
       }}>{"Add"}</Button>
