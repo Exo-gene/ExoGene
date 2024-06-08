d9a0cf59eae57933a6a6d2ba1cb21b6f00a800b4
tiwarNews <168510639+tiwarNews@users.noreply.github.com>
2024-06-08 18:13:46 +0300

    Update +layout.svelte

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -2,8 +2,7 @@
   import { onMount } from "svelte";
   import "../styles.css";
   import "../../app.css";
-  import Navbar from "$lib/components/Navbar.svelte";
-  import Sidebar from "$lib/components/DrawerComponent.svelte";
+  import Layout from "$lib/components/Layout.svelte";
   import LoadingIndicator from "$lib/components/LoadingIndicator.svelte";
   import { goto } from "$app/navigation";
   import { authStore } from "../../stores/Auth.Store";
@@ -28,16 +27,7 @@
 {#if isLoading}
   <LoadingIndicator />
 {:else}
-  <div class="flex min-h-screen">
-    <Sidebar {sidebarOpen} />
-    <div
-      class="flex-1 flex flex-col"
-      style="background-color: var(--background-color); color: var(--text-color)"
-    >
-      <Navbar bind:sidebarOpen />
-      <main class="p-4">
-        <slot></slot>
-      </main>
-    </div>
-  </div>
+  <Layout {sidebarOpen}>
+     <slot />
+  </Layout>
 {/if}
