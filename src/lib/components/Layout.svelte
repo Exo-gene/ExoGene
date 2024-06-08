cc76649eda93b077b2800d3cd8a51dd1aa3bfa1e
tiwarNews <168510639+tiwarNews@users.noreply.github.com>
2024-06-08 18:14:20 +0300

    Create Layout.svelte

---
+++ src/lib/components/Layout.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import Navbar from "$lib/components/Navbar.svelte";
+  import Sidebar from "$lib/components/DrawerComponent.svelte";
+  export let sidebarOpen: boolean = true;
+</script>
+
+<style>
+  .layout {
+    display: grid;
+    grid-template-areas:
+      "navbar navbar"
+      "sidebar main";
+    grid-template-rows: auto 1fr;
+    grid-template-columns: auto 1fr;
+    height: 100vh;
+  }
+
+  .navbar {
+    grid-area: navbar;
+  }
+
+  .sidebar {
+    grid-area: sidebar;
+    transform: translateX(-100%);
+    transition: transform 0.3s ease-in-out;
+  }
+
+  .sidebar.open {
+    transform: translateX(0);
+  }
+
+  .main {
+    grid-area: main;
+    overflow-x: auto;
+    padding: 16px;
+  }
+
+  @media (min-width: 768px) {
+    .sidebar {
+      transform: translateX(0);
+    }
+  }
+</style>
+
+<div class="layout">
+  <div class="navbar">
+    <Navbar bind:sidebarOpen />
+  </div>
+  <div class={`sidebar ${sidebarOpen ? 'open' : ''}`}>
+    <Sidebar {sidebarOpen} />
+  </div>
+  <div class="main" style="background-color: var(--background-color);">
+    <slot />
+  </div>
+</div>
