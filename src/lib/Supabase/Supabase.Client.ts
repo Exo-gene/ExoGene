caf67ccaa81823e30ce3e6218d40af1e233be1f5
Shajwan Namiq <shajwan.namiq2023@gmail.com>
2024-06-26 14:24:30 +0300

    update

--- src/lib/Supabase/Supabase.Client.ts
+++ src/lib/Supabase/Supabase.Client.ts
@@ -3,9 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 export namespace Supabase {
   // console.log("Supabase URL", Enviroments.supabase_url);
-
-  console.log("Supabase URL", Enviroments);
-
+ 
   const supabase = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY);
 
   export const client = supabase;
