d51f49d2fd94b36a12bec214d945b99c141da008
Shajwan2023 <shajwan.namiq2023@gmail.com>
2024-08-05 13:53:57 +0300

    update supabase client

--- src/lib/Supabase/Supabase.Client.ts
+++ src/lib/Supabase/Supabase.Client.ts
@@ -2,7 +2,6 @@ import { VITE_SUPABASE_ANON_KEY, VITE_SUPABASE_URL } from "$env/static/public";
 import { createClient } from "@supabase/supabase-js";
 
 export namespace Supabase {
- 
   const supabase = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY);
 
   export const client = supabase;
